4f485accc48c42c98680179a5fc6b9e68a77607e
Christopher Di Bella <cjdb.ns@gmail.com>
2019-10-24 11:58:14 +1100

    adjusts tests so that main actually returns
    The tests have all code optimised away in a release build, since
    the contracts let the optimiser make assumptions with respect to
    subsequent code.
    
    Closes #5

--- test/fail-ensures.cpp
+++ test/fail-ensures.cpp
@@ -14,9 +14,22 @@
 // limitations under the License.
 //
 #include "cjdb/contracts.hpp"
+#include <cstdio>
+#include <cstdlib>
 
-int main()
+int magic(int x) noexcept
 {
-	auto const x = 0;
-	return CJDB_ENSURES(x != 0), x;
+	return CJDB_ENSURES(x > 1), x * 2;
+}
+
+int main(int const argc, char**)
+{
+	auto const x = magic(argc);
+	if (x == 2) {
+		std::fprintf(stderr, "This should never be printed.\n");
+		std::exit(1);
+	}
+
+	constexpr auto return_code = 255;
+	return return_code;
 }
