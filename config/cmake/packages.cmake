87fcc5fa6a8eb7602216dce9368893bc88ef1912
Christopher Di Bella <cjdb.ns@gmail.com>
2021-01-29 21:15:55 -0800

    adjusts default CI and updates clang-format file

--- config/cmake/packages.cmake
+++ config/cmake/packages.cmake
@@ -1,5 +1,4 @@
 # find_package(absl CONFIG REQUIRED)
-find_package(benchmark CONFIG REQUIRED)
 find_package(Catch2 CONFIG REQUIRED)
 # find_package(constexpr-contracts CONFIG REQUIRED)
 # find_package(fmt CONFIG REQUIRED)
--- config/vcpkg/clang_libstdcxx.cmake
+++ config/vcpkg/clang_libstdcxx.cmake
@@ -13,11 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include("${CMAKE_CURRENT_LIST_DIR}/gnu_base.cmake")
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
 
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_INTERPROCEDURAL_OPTIMIZATION_RELEASE ON)
+
+set(CMAKE_C_COMPILER "clang++")
 set(CMAKE_CXX_COMPILER "clang++")
-set(PROJECT_TEMPLATE_CXX_COMPILER_MINIMUM_VERSION 11)
-
 set(CMAKE_AR "llvm-ar")
 set(CMAKE_RC_COMPILER "llvm-rc")
 set(CMAKE_RANLIB "llvm-ranlib")
@@ -25,7 +30,35 @@ set(CMAKE_RANLIB "llvm-ranlib")
 string(
    JOIN " " CMAKE_CXX_FLAGS
    "${CMAKE_CXX_FLAGS}"
-   -stdlib=libc++
+   -fvisibility=hidden
+   -fstack-protector
+   -fdiagnostics-color=always
+   -pedantic
+   -Werror
+   -Wall
+   -Wextra
+   -Wcast-align
+   -Wconversion
+   -Wdouble-promotion
+   -Wnon-virtual-dtor
+   -Wold-style-cast
+   -Woverloaded-virtual
+   -Wshadow
+   -Wsign-conversion
+   -Wsign-promo
+   -Wunused
+   -Wformat=2
+   -Wodr
+   -Wno-attributes
+   -Wnull-dereference
+   -Wno-unused-command-line-argument
+)
+
+string(
+   JOIN " " CMAKE_CXX_FLAGS_DEBUG
+   "${CMAKE_CXX_FLAGS_DEBUG}"
+   -fsanitize=address,undefined
+   -fstack-protector-strong
 )
 
 string(
@@ -33,6 +66,8 @@ string(
    "${CMAKE_CXX_FLAGS_RELEASE}"
    -fsanitize=cfi
    -fno-sanitize=cfi-unrelated-cast # TODO(cjdb): remove once Catch2 properly supports cfi
+   -fmarch=x86-64-v4
+   -ffast-math
 )
 
 string(
