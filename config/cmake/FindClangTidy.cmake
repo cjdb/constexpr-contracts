88304366f761af477a8ac1065074c2616021f975
Christopher Di Bella <cjdb.ns@gmail.com>
2021-01-30 10:54:56 -0800

    finally cleans up the clang-tidy options

--- config/cmake/FindClangTidy.cmake
+++ config/cmake/FindClangTidy.cmake
@@ -3,25 +3,22 @@
 #
 include(FindPackageHandleStandardArgs)
 
-if((NOT ${${PROJECT_NAME}_ENABLE_CLANG_TIDY}) OR (${PROJECT_NAME}_CLANG_TIDY_PATH STREQUAL ""))
-   if(NOT ${${PROJECT_NAME}_ENABLE_CLANG_TIDY})
-      message(STATUS "clang-tidy disabled.")
-   else()
-      message(STATUS "No path for clang-tidy.")
-   endif()
+if(NOT "${${PROJECT_NAME}_ENABLE_CLANG_TIDY}")
+   message(STATUS "clang-tidy disabled.")
    set(CMAKE_CXX_CLANG_TIDY "")
    return()
 endif()
 
-if(EXISTS "${${PROJECT_NAME}_CLANG_TIDY_PATH}")
+set(clang_tidy_path "${${PROJECT_NAME}_CLANG_TIDY_PATH}")
+
+if(EXISTS "${clang_tidy_path}")
    find_package(Clang REQUIRED)
    set(ClangTidy_FOUND On)
-   set(CMAKE_CXX_CLANG_TIDY "${${PROJECT_NAME}_CLANG_TIDY_PATH}" -p=${CMAKE_BINARY_DIR})
-   message(STATUS "Found ${${PROJECT_NAME}_CLANG_TIDY_PATH}")
+   set(CMAKE_CXX_CLANG_TIDY "${clang_tidy_path}" -p=${CMAKE_BINARY_DIR})
+   message(STATUS "Found ${clang_tidy_path}")
 else()
    set(ClangTidy_FOUND Off)
-   message(STATUS "${PROJECT_NAME}_CLANG_TIDY_PATH=\"${${PROJECT_NAME}_CLANG_TIDY_PATH}\" not found.")
+   message(STATUS "${PROJECT_NAME}_CLANG_TIDY_PATH=\"${clang_tidy_path}\" not found.")
 endif()
 
-find_package_handle_standard_args(ClangTidy
-   REQUIRED_VARS ClangTidy_FOUND)
+find_package_handle_standard_args(ClangTidy REQUIRED_VARS ClangTidy_FOUND)
